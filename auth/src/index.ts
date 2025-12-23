fc7564f23dfb1a083f4596803ae3e54155b78355
Ganwari Nakathige Sakila Lakmal Nandasiri CB014383 <164737611+Sakilalakmal@users.noreply.github.com>
2025-12-23 22:19:50 +0530

    inital app setting commit

---
+++ auth/src/index.ts
@@ -0,0 +1,15 @@
+import express from "express";
+
+//* initalize app
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+//* middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//* start server
+app.listen(PORT, () => {
+  console.log(`Auth service running on port http://localhost${PORT}`);
+});
