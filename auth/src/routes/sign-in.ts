d152e341cefb128542bb6691e32d9a9c67b9e45b
Ganwari Nakathige Sakila Lakmal Nandasiri CB014383 <164737611+Sakilalakmal@users.noreply.github.com>
2025-12-25 01:22:56 +0530

    Implement request validation middleware and enhance sign-in/sign-up routes

--- auth/src/routes/sign-in.ts
+++ auth/src/routes/sign-in.ts
@@ -1,10 +1,58 @@
-import express from "express";
+import express, { type Request, type Response } from "express";
+import { body } from "express-validator";
+import { validateRequest } from "../middlewares/validate-request.js";
+import { User } from "../models/User.js";
+import { BadRequestError } from "../errors/request-validation-error.js";
+import { Password } from "../services/password.js";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
 //* current user route *//
-router.post("/api/users/signin", (req, res) => {
-    res.send('Hi there from current user route');
-});
+router.post(
+  "/api/users/signin",
+  [
+    body("email").isEmail().withMessage("Email must be valid"),
+    body("password")
+      .trim()
+      .notEmpty()
+      .withMessage("provide password to sign in"),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+
+    if (!existingUser) {
+      throw new BadRequestError("Invalid Credentials");
+    }
+
+    const passwordMatch = await Password.compare(
+      existingUser.password,
+      password
+    );
+
+    if (!passwordMatch) {
+      throw new BadRequestError("Invalid Credentials");
+    }
+
+    //* generate jsonweb token
+    const userJwt = jwt.sign(
+      {
+        id: existingUser.id,
+        email: existingUser.email,
+      },
+      process.env.JWT_KEY!
+    );
+    //* store token in session object
+
+    req.session = {
+      jwt: userJwt,
+    };
+
+    res.status(200).send(existingUser);
+  }
+);
 
 export { router as signInRouter };
